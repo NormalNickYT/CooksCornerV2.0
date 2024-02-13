a90917b4166951ccfd17c87bbb506fb58ed3be39
NickTheSmartestGuyAlive <nickkoster2001@hotmail.com>
2024-02-13 17:14:29 +0100

    feature: Added Prisma and made connection with PlanetScale

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,18 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1> </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <Input type = "email" placeholder = "Email" />
         <Button> Click Me </Button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p>  
       </p>
     </>
   )
--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
+import { PrismaClient } from '@prisma/client'
 const express = require('express')
 const app = express()
+const prisma = new PrismaClient();
+
 const port = 5000
 
-app.get('/api', (req, res) => {
-    res.json({"users": ["BackEndTest1", "BackEndTest2", "BackEndTest3"]})
-  })
+app.get('/api/users', async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
 
 app.listen(port, () => {
-console.log(`Server Started On: ${port}`)
+  console.log(`Server Started On: ${port}`)
 })
+
