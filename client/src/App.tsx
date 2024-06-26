15aa703b7ef5b6e2f1dd283bfa13f01736717497
NickTheSmartestGuyAlive <nickkoster2001@hotmail.com>
2024-06-26 09:39:26 +0200

    FEAT: Footer added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,42 +1,52 @@
-import Navbar from "./components/Navbar"
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Dashboard from "./pages/Dashboard"; 
-import { Layout } from "./layouts/layout"
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import { Layout } from "./layouts/layout";
 import AuthProvider from "./hooks/AuthProvider";
 import PrivateRoute from "./components/PrivateRoutes";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-      <AuthProvider>
-      <Navbar />
-        <Routes> 
-          <Route path="/" element={ 
-            <Layout>
-              <Home />
-            </Layout>
-          } />
-          <Route path="/Login" element={
-            <Layout>
-              <Login />
-            </Layout>
-          } 
-          />
-          <Route element = {<PrivateRoute/>}>
-            <Route path = "/dashboard" element = {
-              <Layout>
-                <Dashboard />
-              </Layout>
-            }/>
-          </Route>
-        </Routes> 
-      </AuthProvider>
-    </BrowserRouter>
+      <BrowserRouter>
+        <AuthProvider>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Layout>
+                  <Home />
+                </Layout>
+              }
+            />
+            <Route
+              path="/Login"
+              element={
+                <Layout>
+                  <Login />
+                </Layout>
+              }
+            />
+            <Route element={<PrivateRoute />}>
+              <Route
+                path="/dashboard"
+                element={
+                  <Layout>
+                    <Dashboard />
+                  </Layout>
+                }
+              />
+            </Route>
+          </Routes>
+          <Footer />
+        </AuthProvider>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
